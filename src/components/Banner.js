ce9daafafd51dc9b76acecc804e537750317ef50
Edwin Rodriguez <RodriguezEdwin1324@gmail.com>
2021-08-20 16:41:22 -0400

    FE: Completed banner component. Starting Row component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/modules/Banner.module.css";
+import axios from "../api/axios";
+import requests from "../api/Requests";
 
 export default function Banner() {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const {
+          data: { results },
+        } = await axios(requests.fetchTrending);
+
+        setMovie(results[Math.floor(Math.random() * results.length - 1)]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substring(0, n - 1) + "..." : string;
+  };
+
   return (
-    <div className="banner">
-      <h1>tester</h1>
-    </div>
+    <header
+      className={styles.banner}
+      style={{
+        backgroundSize: "max(100vw, 900px)",
+        backgroundRepeat: "no-repeat",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center top",
+      }}
+    >
+      <div className={styles.banner_contents}>
+        <h1 className={styles.banner_title}>
+          {movie?.title || movie?.original_title}
+        </h1>
+        <h1 className={styles.banner_description}>
+          {truncate(movie?.overview, 200)}
+        </h1>
+        <div className={styles.banner_buttons}>
+          <button
+            className={`${styles.banner_button} ${styles.banner_play_button}`}
+          >
+            Play
+          </button>
+          <button
+            className={`${styles.banner_button} ${styles.banner_info_button}`}
+          >
+            More Info
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.fade_bottom} />
+    </header>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Nav, Banner } from "../components";
+import { Nav, Banner, Row } from "../components";
+import requests from "../api/Requests";
 
 export default function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
